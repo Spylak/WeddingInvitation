84aea8ab12f16e18eff12c2fc61bef5dc83729f7
mrspyretos <spylak@hotmail.com>
2022-03-22 21:15:56 +0200

    Added API project

---
+++ InvitationAPI/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvitationAPI.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class AdminController : ControllerBase
+{
+    private readonly ILogger<AdminController> _logger;
+    private readonly IConfiguration _configuration;
+    public AdminController(ILogger<AdminController> logger,IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+    [HttpGet(Name = "Authenticate")]
+    public string GetToken(string usernameInput,string passwordInput)
+    {
+        var username=_configuration.GetValue<string>("AdminCreds:Username");
+        var password=_configuration.GetValue<string>("AdminCreds:Password");
+        if (username == usernameInput && passwordInput == password)
+        {
+            return "Success";
+        }
+        return "Unauthorized";
+    }
+}
---
+++ InvitationAPI/Controllers/EmailController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvitationAPI.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class EmailController : ControllerBase
+{
+    private readonly ILogger<EmailController> _logger;
+    private readonly IConfiguration _configuration;
+    public EmailController(ILogger<EmailController> logger,IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+    [HttpPost(Name = "Authenticate")]
+    public async Task<IActionResult> GetToken(string email)
+    {
+        
+        return Ok();
+    }
+}
